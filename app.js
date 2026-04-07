b794cbb34e8acac6e9705c17a8f461c5f73b3b74
ruthadama28-dev <ruthadama28@gmail.com>
2026-04-07 16:06:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const projects = [
   {
     name: "Pharmacy Ordering System",
-    desc: "A web application for ordering pharmaceutical products with cart and tracking features."
+    desc: "A web app for ordering pharmaceutical products",
+    github: "https://github.com/ruthadama28-dev/pharmacy-app"
   },
   {
     name: "Bug Tracking System",
-    desc: "A system for logging, tracking, and resolving software bugs."
+    desc: "A system for tracking software bugs",
+    github: "https://github.com/ruthadama28-dev/bug-tracker"
   }
 ];
 
@@ -13,9 +15,14 @@ const container = document.getElementById("projects");
 
 projects.forEach(project => {
   const div = document.createElement("div");
+
   div.innerHTML = `
     <h3>${project.name}</h3>
     <p>${project.desc}</p>
+    <a href="https://ruthadama28-dev.github.io/pharmacy-app/" target="_blank">
+  View Project
+</a>
   `;
+
   container.appendChild(div);
 });
