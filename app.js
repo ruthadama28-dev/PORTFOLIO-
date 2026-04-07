6dafde9961756fce02d5ee0d925634a0c0eff705
ruthadama28-dev <ruthadama28@gmail.com>
2026-04-07 16:12:39 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const projects = [
   {
     name: "Pharmacy Ordering System",
     desc: "A web app for ordering pharmaceutical products",
-    github: "https://github.com/ruthadama28-dev/pharmacy-app"
+    live: "https://ruthadama28-dev.github.io/pharmacy-app/"
   },
   {
     name: "Bug Tracking System",
     desc: "A system for tracking software bugs",
-    github: "https://github.com/ruthadama28-dev/bug-tracker"
+    live: "https://ruthadama28-dev.github.io/bug-tracker/"
   }
 ];
 
@@ -15,13 +15,12 @@ const container = document.getElementById("projects");
 
 projects.forEach(project => {
   const div = document.createElement("div");
+  div.classList.add("project-card"); // optional for styling
 
   div.innerHTML = `
     <h3>${project.name}</h3>
     <p>${project.desc}</p>
-    <a href="https://ruthadama28-dev.github.io/pharmacy-app/" target="_blank">
-  View Project
-</a>
+    <a href="${project.live}" target="_blank">View Project</a>
   `;
 
   container.appendChild(div);
